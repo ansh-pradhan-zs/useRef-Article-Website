0f8d858f52c2a05783b035d31acef1be34723065
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-02-03 08:06:27 +0530

    made the website tablet responsive

--- components/Example.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useRef } from "react";
-
-const ExampleComponent = () => {
-  const divRef = useRef<HTMLDivElement>(null);
-
-  return <div ref={divRef}>ExampleComponent</div>;
-};
-
-export default ExampleComponent;
--- components/ScrollToNavigator/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./scrolltonavigator.module.css";
-
-const ScrollToNavigator = () => {
-  return <div></div>;
-};
-
-export default ScrollToNavigator;
